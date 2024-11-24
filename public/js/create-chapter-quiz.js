cbc1277bb639bfc32d0a5c1957165168af99e023
Craig Tanka Mudzingwa <mudzingwacra@gmail.com>
2024-11-24 22:27:08 +0100

    added ability to add multiple questions

--- public/js/create-chapter-quiz.js
+++ public/js/create-chapter-quiz.js
@@ -1,10 +1,14 @@
-const quizAnswersContainer = document.querySelector('.quiz-answers-answers')
-const addAnswerBtn = document.querySelector('.add-answer-btn')
+const allQuestionsContainer = document.querySelector('.all-questions-container')
+const addQuestionBtn = document.querySelector('.add-question-btn')
 
-addAnswerBtn.addEventListener('click', (event) => {
-        // alert('bho here')
+let numOfQuestions = 1
+let currentQuestion = 1
+
+allQuestionsContainer.addEventListener('click', function (event) {
         event.preventDefault()
-        
+        if (event.target.classList.contains('add-answer-btn')) addAnswerToQuestion(event.target)
+})
+function addAnswerToQuestion(pressedElement) {
         //create new answer container element
         const newAnswerCont = document.createElement('span')
         newAnswerCont.classList.add('added-answer-input-cont')
@@ -25,7 +29,38 @@ addAnswerBtn.addEventListener('click', (event) => {
         newAnswerCont.appendChild(removeAnswerBtn)
 
         // add the new answer container to the dom right before the add answer element
-        quizAnswersContainer.insertBefore(newAnswerCont, addAnswerBtn)
+        const questionAnswersContainer = pressedElement.closest('.question-answers-container')
+        questionAnswersContainer.insertBefore(newAnswerCont, pressedElement)
 
         // TODO: add remove answer event listener to remove answer button
+}
+
+addQuestionBtn.addEventListener('click', (event) => {
+        event.preventDefault()
+        
+        numOfQuestions += 1
+        //create new questions element
+        const newQuestion = document.createElement('div')
+        newQuestion.classList.add('question-container')
+        newQuestion.id = `qn-${numOfQuestions}`
+
+        //set the inner html  of new element
+        newQuestion.innerHTML = 
+                `<div class="question-expanded">
+                        <div class="form-row">
+                                <label class="form-label">Question <span class="question-number">${numOfQuestions}</span></label>
+                                <input type="text" id="youtube-video-link-input" class="quiz-question-input">
+                        </div>
+                        <fieldset class="quiz-answers">
+                                <legend>Answers:</legend>
+                                <div class="question-answers-container">
+                                        <input type="text" class="answer-input">
+                                        <input type="text" class="answer-input">
+                                        <button class="add-answer-btn">+</button>
+                                </div>
+                        </fieldset>
+                </div>`;
+
+        // append the new question to the dom
+        allQuestionsContainer.appendChild(newQuestion)
 })
