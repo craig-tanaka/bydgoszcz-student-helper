12d923f1addf486be29ca84f1a8c8a1c52acdd9d
Craig Tanka Mudzingwa <mudzingwacra@gmail.com>
2024-11-25 02:21:50 +0100

    Added Quiz Preview Functionality

--- public/js/create-chapter-quiz.js
+++ public/js/create-chapter-quiz.js
@@ -9,7 +9,13 @@ allQuestionsContainer.addEventListener('click', function (event) {
         if (event.target.classList.contains('add-answer-btn')) addAnswerToQuestion(event.target)
         if(event.target.classList.contains('maximize-question-btn')) maximizeQuestion(event.target)
 })
+allQuestionsContainer.addEventListener('input', (event)=> {
+        updatePreview(event.target)
+})
+
 function addAnswerToQuestion(pressedElement) {
+        const questionAnswersContainer = pressedElement.closest('.question-answers-container')
+
         //create new answer container element
         const newAnswerCont = document.createElement('span')
         newAnswerCont.classList.add('added-answer-input-cont')
@@ -19,6 +25,10 @@ function addAnswerToQuestion(pressedElement) {
         answerInput.type = 'text'
         answerInput.classList.add('answer-input')
         answerInput.classList.add('added')
+
+        // find the number of the last answer element to determine the number the next should have
+        const answerNumber = questionAnswersContainer.querySelectorAll('input.answer-input').length
+        answerInput.id = `qn-${currentQuestion}-answer-input-${answerNumber + 1}`
 
         //create remove answer element
         const removeAnswerBtn = document.createElement('button')
@@ -30,10 +40,26 @@ function addAnswerToQuestion(pressedElement) {
         newAnswerCont.appendChild(removeAnswerBtn)
 
         // add the new answer container to the dom right before the add answer element
-        const questionAnswersContainer = pressedElement.closest('.question-answers-container')
         questionAnswersContainer.insertBefore(newAnswerCont, pressedElement)
-
+        addanswerToPreview()
         // TODO: add remove answer event listener to remove answer button
+}
+function addanswerToPreview() {
+        const previewQuestionsContainer = document.querySelector('.questions-preview-cont')
+        const questionPreviewCont = previewQuestionsContainer.querySelector(`#preview-qn-${currentQuestion}`)
+        const answersContainer = questionPreviewCont.querySelector('.question-answers-preview')
+        
+        // the number of the new answer being made
+        const answerNumber = answersContainer.childElementCount + 1
+
+        const newAnswerElement = document.createElement('div')
+        newAnswerElement.classList.add('preview-answer-cont')
+        newAnswerElement.innerHTML =
+                `<div class="preview-answer-cont">
+                        <input type="radio" class="radio-answer-input" ansNum="${answerNumber}" name="preview-ans-${currentQuestion}" value="" />
+                        <label class="radio-answer-label">...</label>
+                </div>`
+        answersContainer.appendChild(newAnswerElement)
 }
 
 addQuestionBtn.addEventListener('click', (event) => {
@@ -55,8 +81,8 @@ addQuestionBtn.addEventListener('click', (event) => {
                         <fieldset class="quiz-answers">
                                 <legend>Answers:</legend>
                                 <div class="question-answers-container">
-                                        <input type="text" class="answer-input">
-                                        <input type="text" class="answer-input">
+                                        <input type="text" class="answer-input" id="qn-${numOfQuestions}-answer-input-1">
+                                        <input type="text" class="answer-input" id="qn-${numOfQuestions}-answer-input-1">
                                         <button class="add-answer-btn">+</button>
                                 </div>
                         </fieldset>
@@ -66,7 +92,32 @@ addQuestionBtn.addEventListener('click', (event) => {
         allQuestionsContainer.appendChild(newQuestion)
         minmizePreviousQuestion()
         currentQuestion = numOfQuestions
+        addQuestionToPreview();
 })
+function addQuestionToPreview(){
+        const previewQuestionsContainer = document.querySelector('.questions-preview-cont')
+
+        //create new question preview element
+        const questionContainer = document.createElement('div')
+        questionContainer.classList.add('question-preview-single')
+        questionContainer.id = `preview-qn-${currentQuestion}`
+        questionContainer.innerHTML =
+                `<p class="question"></p>
+                <fieldset class="question-answers-preview">
+                        <div class="preview-answer-cont">
+                                <input type="radio" class="radio-answer-input" ansNum="1" name="preview-ans-${currentQuestion}" value="" />
+                                <label class="radio-answer-label">...</label>
+                        </div>
+                        <div class="preview-answer-cont">
+                                <input type="radio" class="radio-answer-input" ansNum="2" name="preview-ans-${currentQuestion}" value="" />
+                                <label class="radio-answer-label">...</label>
+                        </div>
+                </fieldset>`
+        
+        previewQuestionsContainer.append(questionContainer)
+
+}
+
 function minmizePreviousQuestion() {
         const currentQuestionElement = document.querySelector(`#qn-${currentQuestion}`)
 
@@ -102,4 +153,18 @@ function maximizeQuestion(pressedElement) {
         // hide the minimized version of the question and unhide the expanded one
         questionContainer.querySelector('.question-expanded').classList.remove('hidden')
         questionContainer.querySelector('.question-minimized').classList.add('hidden')
+}
+
+function updatePreview(typedElement) {
+        const previewQuestionsContainer = document.querySelector('.questions-preview-cont')
+        const questionPreviewCont = previewQuestionsContainer.querySelector(`#preview-qn-${currentQuestion}`)
+
+        if (typedElement.classList.contains('quiz-question-input')) {
+                questionPreviewCont.querySelector('.question').innerHTML = typedElement.value
+        } else if (typedElement.classList.contains('answer-input')) {
+                const answerlabels = questionPreviewCont.querySelectorAll('.radio-answer-label');
+                const answerNumber = typedElement.id[typedElement.id.length -1]
+                
+                answerlabels[answerNumber - 1].innerHTML = typedElement.value
+        }
 }
