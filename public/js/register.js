97a2e5f0b26462a085bd83e547c83b68e24f4701
Craig Tanka Mudzingwa <mudzingwacra@gmail.com>
2024-12-10 13:28:39 +0100

    create user doc on user registration

--- public/js/register.js
+++ public/js/register.js
@@ -2,42 +2,88 @@ const error_label = document.querySelector('#register-error-label')
 const submit_btn = document.querySelector('#register-form-submit')
 
 let email, password, repeat_password;
+const db = firebase.firestore()
 
 submit_btn.addEventListener('click', (e) => {
         e.preventDefault()
 
+        userName = document.querySelector("input[name='name']").value;
         email = document.querySelector("input[name='email']").value;
         password = document.querySelector("input[name='password']").value;
         repeat_password = document.querySelector("input[name='repeat-password']").value;
 
-        if (password !== repeat_password) {
-                error_label.innerHTML = 'Passwords Must Match';
-                error_label.style.color = 'red';
-                error_label.classList.remove('hidden');
-                return;
-        }
-        if (password.length < 8 ) {
-                error_label.innerHTML = 'Password must be at least 8 characters';
-                error_label.style.color = 'red';
-                error_label.classList.remove('hidden');
-                return;
-        }
-        
-        error_label.style.color = 'red';
-        error_label.classList.remove('hidden');
+        if (!validateForm()) return;
+
+        submit_btn.classList.add('hidden')
 
         firebase.auth().createUserWithEmailAndPassword(email, password)
                 .then((userCredential) => {
-                        // Signed in 
-                        console.log(userCredential)
-                        alert('user created');
-                        // ...
+                        createUserDocument(userCredential.user)
                 })
                 .catch((error) => {
                         let errorCode = error.code;
                         let errorMessage = error.message;
-                        alert('user creation failed');
-                        // ..
+
+                        // console.log('Error ' + errorCode + ': ' + errorMessage)
+
+                        error_label.innerHTML = "Failed To Register, Please Try again after a while."
+                        error_label.style.color = '#b14e4e';
+                        submit_btn.classList.remove('hidden')
                 });
 
 })
+
+function validateForm() {
+        let userName = document.querySelector("input[name='name']")
+        let userEmail = document.querySelector("input[name='email']")
+        let password = document.querySelector("input[name='password']")
+        let repeat_password = document.querySelector("input[name='repeat-password']")
+
+        let isFormValid = true
+
+        if (password.value !== repeat_password.value) {
+                password.style.outline = "1px Solid #b14e4e"
+                repeat_password.style.outline = "1px Solid #b14e4e"
+                error_label.innerHTML = 'Passwords Must Match';
+                isFormValid = false
+        } else {
+                password.style.outline = "initial"
+                repeat_password.style.outline = "initial"
+        }
+
+        if (password.value.length < 8) {
+                password.style.outline = "1px Solid #b14e4e"
+                error_label.innerHTML = 'Password must be at least 8 characters';
+                isFormValid = false
+        } else password.style.outline = "initial"
+
+        if ( !userEmail.value.includes('@') || !userEmail.value.includes('.') || userEmail.value === "") {
+                userEmail.style.outline = "1px Solid #b14e4e"
+                error_label.innerHTML = 'Please Enter A valid Email';
+                isFormValid = false
+        } else userEmail.style.outline = "initial"
+
+        if (userName.value === "") {
+                userName.style.outline = "1px Solid #b14e4e"
+                error_label.innerHTML = 'Please Fill Out Missing Fields';
+                isFormValid = false
+        } else userName.style.outline = "initial"
+        
+        if (!isFormValid) error_label.style.color = '#b14e4e';
+        else error_label.style.color = 'transparent'
+        
+        return isFormValid
+}
+
+function createUserDocument(user) {
+        userName = document.querySelector("input[name='name']").value;
+        userEmail = document.querySelector("input[name='email']").value;
+
+        db.collection('users').doc(user.uid).set({
+                name: userName,
+                email: userEmail,
+                isAdmin: false
+        }).then(doc => {
+                window.location.href = './home.html'
+        })
+}
